f12548d944f8f81d407fb68bba1a9f8f8f937a1f
codewithcojo <breigns60@gmail.com>
2026-08-12 11:07:12 +0000

    fixed errors

---
+++ src/components/admin/index.ts
@@ -0,0 +1,22 @@
+// src/components/admin/index.ts
+// Dashboard
+export { StatsCards } from "./Dashboard/StatsCards";
+export { FaultyVotesChart } from "./Dashboard/FaultyVotesChart";
+export { LiveUpdates } from "./Dashboard/LiveUpdates";
+export { SessionProgress } from "./Dashboard/SessionProgress";
+export { GenderStats } from "./Dashboard/GenderStats";
+export { TurnoverChart } from "./Dashboard/TurnoverChart";
+export { AdminDashboard } from "./Dashboard/AdminDashboard";
+
+// Candidates
+export { CandidateForm } from "./Candidates/CandidateForm";
+export { CandidateList } from "./Candidates/CandidateList";
+export { CandidateManagement } from "./Candidates/CandidateManagement";
+
+// Sessions
+export { SessionForm } from "./Sessions/SessionForm";
+export { SessionManagement } from "./Sessions/SessionManagement";
+
+// Voters
+export { VoterManagement } from "./Voters/VoterManagement";
+export { VoterUpload } from "./Voters/VoterUpload";
