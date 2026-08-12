9c71e9e711446f23beafc55750c088d62d39980b
codewithcojo <breigns60@gmail.com>
2026-08-12 14:38:56 +0000

    fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { AppRoutes } from "./routes/AppRoutes";
-import "./styles/tailwind.css";
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
